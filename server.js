17ed1418728a7826bf321d324d873a524421047d
Matheusdfr93 <matheusdfr93@gmail.com>
2019-11-22 15:45:45 -0300

    add buttons

--- server.js
+++ server.js
@@ -28,23 +28,76 @@ var context = {};
 var telegramBot = new tbot('970020294:AAFTqOQAlL3qAE41XtzGrpdmVtnyfSnLKrw', 
 { polling: true });
 
+function sendMessage(context, msg, ses) {
+	return conversation.message({
+		assistantId: '422166ea-c502-4b2a-bbc9-4c5c081b1146',
+		sessionId: ses,
+		context: context,
+		input: {
+		  'message_type': 'text',
+		  'text': msg
+		  }
+		})
+}
+
+telegramBot.on('callback_query', (callbackQuery) => {
+	const message = callbackQuery.message;
+	const category = callbackQuery.data;
+ 
+	//URLLabels.push({
+	//	url: tempSiteURL,
+	//	label: category,
+	//});
+ 
+	//tempSiteURL = '';
+	ses.then(res => {
+		sendMessage(context, category, res.result.session_id ).then(res =>{
+			telegramBot.sendMessage(message.chat.id, res.result.output.generic[0].text);
+			console.log(message.chat.id)
+		})
+	
+	})
+	
+ });
+
 telegramBot.on('message', function (msg) {
 	var chatId = msg.chat.id;	
 	
 	ses.then(res => {
 			console.log(res.result.session_id)
-			conversation.message({
-				assistantId: '422166ea-c502-4b2a-bbc9-4c5c081b1146',
-				sessionId: res.result.session_id,
-				context: context,
-				input: {
-				  'message_type': 'text',
-				  'text': msg.text
-				  }
-				})
+			sendMessage(context, msg.text, res.result.session_id) 
 				.then(res => {
+					const type = res.result.output.generic[0].response_type;
 					context = res.result.context;
-					telegramBot.sendMessage(chatId, res.result.output.generic[0].text);
+					switch (type) {
+						case 'text':
+							console.log(JSON.stringify(res.result.output.generic[0].response_type));
+							telegramBot.sendMessage(chatId, res.result.output.generic[0].text);
+							break;
+
+						case 'option':
+							const options = res.result.output.generic[0].options;
+							const jOptions =   {
+								reply_markup: {
+									inline_keyboard: [[
+										{
+											text: options[0].label,
+											callback_data: options[0].label
+										}, {
+											text: options[1].label,
+											callback_data: options[1].label
+										},
+									]]
+								}
+							}
+								console.log(JSON.stringify(res.result.output.generic[0].response_type));
+								telegramBot.sendMessage(chatId, res.result.output.generic[0].title, jOptions);
+								break;
+
+						default:
+							break;
+					}
+
 				})
 				.catch(err => {
 				  console.log(err);
