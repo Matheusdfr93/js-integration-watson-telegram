11dd2ee63a8a2378f70c91797cc65b0d113174aa
Matheusdfr93 <matheusdfr93@gmail.com>
2019-12-06 18:19:47 -0300

    gravar questionario no banco de dados

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const cfenv = require('cfenv');
 const apiKeys = require('./app.json');
 const {Pool, Client} = require('pg');
-
 var watson = require('watson-developer-cloud');
 var tbot = require('node-telegram-bot-api');
-
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -23,16 +21,14 @@ pguser.connect();
 c = pguser
 .query('SELECT * from segurança;')
 .then(res => console.log(res.rows[0]))
-.catch(e => console.error(e.stack))
+.catch(e => console.error(e.stack));
 
-console.log('c =',c)
+console.log('c =',c);
 
 const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
-
-let acessPermission = [804932589, 740600431, 710342198];
-
+let acessPermission = [804932589, 740600431, 710342198,905858684];
 
 const conversation = new AssistantV2({
   version: apiKeys.watsonVersion,
@@ -41,7 +37,6 @@ const conversation = new AssistantV2({
   }),
   url: 'https://gateway.watsonplatform.net/assistant/api',
 });
-
 
 async function newWatsonSes() {
 	return await conversation.createSession({
@@ -75,57 +70,64 @@ async function deleteSession (user){
 	userSession.pop(findUser)
 }
 
-var context = {};
-var telegramBot = new tbot(apiKeys.apiTelegram, 
+const userContext = {};
+const telegramBot = new tbot(apiKeys.apiTelegram, 
  { polling: true });
 
- 
-function sendMessage(context, msg, ses) {
+function sendMessage(msg, ses) {
 	console.log(msg)
 	return conversation.message({
 		assistantId: apiKeys.assistantId,
 		sessionId: ses,
-		context: context,
 		input: {
 		  'message_type': 'text',
-		  'text': msg
-		}
+		  'text': msg,
+			options: {
+				return_context: true
+			},
+		},
+		context: {
+      global: {
+        system: {
+          user_id: ses
+        }
+      },
+      skills: {
+        'main skill': {
+          user_defined: {}
+        }
+      }
+    }
 	})
 }
-
-telegramBot.on('callback_query', (callbackQuery) => {
-	const message = callbackQuery.message;
-	let armazena;
-	const category = callbackQuery.data;
-	const session = createSession(message.from.id)
-
-	sendMessage(context, category, session ).then(res =>{
-		telegramBot.sendMessage(message.chat.id, res.result.output.generic[0].text);
-		console.log(message.chat.id)
-	})
-	
- });
  
- let auxValores = 0;
+let auxValores = 0;
+let valores = [];
 telegramBot.on('message', function (msg) {
-	let valores = [];
 	var chatId = msg.chat.id;	
 	console.log("user: ", );
 	const session = createSession(msg.from.id);
 	session.then(ses => {
 		console.log('retorno: ',ses)
-		sendMessage(context, msg.text, ses) 
+		sendMessage(msg.text, ses) 
 			.then(res => {
 				const type = res.result.output.generic[0].response_type;
-				context = res.result.context;
-				if(msg.text == 'Bebidas'){
-					auxValores = 1;
-				} 
-				console.log('aux: ', auxValores);
-				if(auxValores == 1){
-					valores.push(msg.text);
-					console.log('valores: ',valores);
+				const watsonContext = res.result.context.skills['main skill'].user_defined;
+				if(watsonContext) {
+					if(watsonContext.computar === 1) {
+						console.log(watsonContext.computar)
+						const query = {
+							text: 'INSERT INTO validacao (tipoValidacao, departamento, sku, temproduto, motivo_nao_venda, conseguiu_ajustar, solicitarAbastecimento) values ($1, $2, $3, $4, $5, $6, $7);	',
+							values: [watsonContext.tipoValidacao, watsonContext.departamento, watsonContext.sku, watsonContext.temproduto, watsonContext.motivonaovenda, watsonContext.conseguiuAjustar, watsonContext.solicitarAbastecimento],
+						}
+						c = pguser
+						.query(query)
+						//.then(res => console.log('Valores Computados'))
+						//.catch(e => console.error(e.stack));
+					}
+					console.log('resultado:',  watsonContext);
 				}
+
 				if (acessPermission.includes(msg.from.id)){
 				switch (type) {
 					case 'text':
@@ -166,6 +168,7 @@ telegramBot.on('message', function (msg) {
 				} 
 			})
 			.catch(err => {
+				console.log(err)
 				deleteSession(msg.from.id)
 				telegramBot.sendMessage(chatId, 'Sua sessão expirou, me mande um novo olá, por favor');
 			})
